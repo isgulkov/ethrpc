ad78433b4dd29599bf4d5b398ff81d66400f9540
Ilya Gulkov <x@isgulkov.ru>
2018-08-01 13:49:54 +0300

    Replace a good two thirds of methods with __call overload (L M A O)

--- src/Lib/Ethereum.php
+++ src/Lib/Ethereum.php
@@ -28,59 +28,16 @@ class Ethereum extends JsonRPC
         return $input;
     }
 
-    function web3_clientVersion()
+    function __call($name, $arguments)
     {
-        return $this->ether_request(__FUNCTION__);
-    }
+        // Nigga, what?
 
-    function web3_sha3($input)
-    {
-        return $this->ether_request(__FUNCTION__, array($input));
-    }
-
-    function net_version()
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
-    function net_listening()
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
-    function net_peerCount()
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
-    function eth_protocolVersion()
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
-    function eth_coinbase()
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
-    function eth_mining()
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
-    function eth_hashrate()
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
-    function eth_gasPrice()
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
-    function eth_accounts()
-    {
-        return $this->ether_request(__FUNCTION__);
+        if(count($arguments) !== 0) {
+            return $this->ether_request($name, $arguments);
+        }
+        else {
+            return $this->ether_request($name);
+        }
     }
 
     function eth_blockNumber($decode_hex=FALSE)
@@ -118,19 +75,9 @@ class Ethereum extends JsonRPC
         return $count;
     }
 
-    function eth_getBlockTransactionCountByHash($tx_hash)
-    {
-        return $this->ether_request(__FUNCTION__, array($tx_hash));
-    }
-
     function eth_getBlockTransactionCountByNumber($tx='latest')
     {
         return $this->ether_request(__FUNCTION__, array($tx));
-    }
-
-    function eth_getUncleCountByBlockHash($block_hash)
-    {
-        return $this->ether_request(__FUNCTION__, array($block_hash));
     }
 
     function eth_getUncleCountByBlockNumber($block='latest')
@@ -141,11 +88,6 @@ class Ethereum extends JsonRPC
     function eth_getCode($address, $block='latest')
     {
         return $this->ether_request(__FUNCTION__, array($address, $block));
-    }
-
-    function eth_sign($address, $input)
-    {
-        return $this->ether_request(__FUNCTION__, array($address, $input));
     }
 
     function eth_sendTransaction($transaction)
@@ -194,56 +136,6 @@ class Ethereum extends JsonRPC
         return $this->ether_request(__FUNCTION__, array($block, $full_tx));
     }
 
-    function eth_getTransactionByHash($hash)
-    {
-        return $this->ether_request(__FUNCTION__, array($hash));
-    }
-
-    function eth_getTransactionByBlockHashAndIndex($hash, $index)
-    {
-        return $this->ether_request(__FUNCTION__, array($hash, $index));
-    }
-
-    function eth_getTransactionByBlockNumberAndIndex($block, $index)
-    {
-        return $this->ether_request(__FUNCTION__, array($block, $index));
-    }
-
-    function eth_getTransactionReceipt($tx_hash)
-    {
-        return $this->ether_request(__FUNCTION__, array($tx_hash));
-    }
-
-    function eth_getUncleByBlockHashAndIndex($hash, $index)
-    {
-        return $this->ether_request(__FUNCTION__, array($hash, $index));
-    }
-
-    function eth_getUncleByBlockNumberAndIndex($block, $index)
-    {
-        return $this->ether_request(__FUNCTION__, array($block, $index));
-    }
-
-    function eth_getCompilers()
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
-    function eth_compileSolidity($code)
-    {
-        return $this->ether_request(__FUNCTION__, array($code));
-    }
-
-    function eth_compileLLL($code)
-    {
-        return $this->ether_request(__FUNCTION__, array($code));
-    }
-
-    function eth_compileSerpent($code)
-    {
-        return $this->ether_request(__FUNCTION__, array($code));
-    }
-
     function eth_newFilter($filter, $decode_hex=FALSE)
     {
         if(!is_a($filter, EthereumFilter::class))
@@ -281,21 +173,6 @@ class Ethereum extends JsonRPC
         return $id;
     }
 
-    function eth_uninstallFilter($id)
-    {
-        return $this->ether_request(__FUNCTION__, array($id));
-    }
-
-    function eth_getFilterChanges($id)
-    {
-        return $this->ether_request(__FUNCTION__, array($id));
-    }
-
-    function eth_getFilterLogs($id)
-    {
-        return $this->ether_request(__FUNCTION__, array($id));
-    }
-
     function eth_getLogs($filter)
     {
         if(!is_a($filter, EthereumFilter::class))
@@ -306,41 +183,6 @@ class Ethereum extends JsonRPC
         {
             return $this->ether_request(__FUNCTION__, $filter->toArray());
         }
-    }
-
-    function eth_getWork()
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
-    function eth_submitWork($nonce, $pow_hash, $mix_digest)
-    {
-        return $this->ether_request(__FUNCTION__, array($nonce, $pow_hash, $mix_digest));
-    }
-
-    function db_putString($db, $key, $value)
-    {
-        return $this->ether_request(__FUNCTION__, array($db, $key, $value));
-    }
-
-    function db_getString($db, $key)
-    {
-        return $this->ether_request(__FUNCTION__, array($db, $key));
-    }
-
-    function db_putHex($db, $key, $value)
-    {
-        return $this->ether_request(__FUNCTION__, array($db, $key, $value));
-    }
-
-    function db_getHex($db, $key)
-    {
-        return $this->ether_request(__FUNCTION__, array($db, $key));
-    }
-
-    function shh_version()
-    {
-        return $this->ether_request(__FUNCTION__);
     }
 
     function shh_post($post)
@@ -355,58 +197,13 @@ class Ethereum extends JsonRPC
         }
     }
 
-    function shh_newIdentity()
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
-    function shh_hasIdentity($id)
-    {
-        return $this->ether_request(__FUNCTION__);
-    }
-
     function shh_newFilter($to=NULL, $topics=array())
     {
         return $this->ether_request(__FUNCTION__, array(array('to'=>$to, 'topics'=>$topics)));
     }
 
-    function shh_uninstallFilter($id)
-    {
-        return $this->ether_request(__FUNCTION__, array($id));
-    }
-
-    function shh_getFilterChanges($id)
-    {
-        return $this->ether_request(__FUNCTION__, array($id));
-    }
-
-    function shh_getMessages($id)
-    {
-        return $this->ether_request(__FUNCTION__, array($id));
-    }
-
-    function personal_newAccount($passphrase){
-        return $this->ether_request(__FUNCTION__, array($passphrase));
-    }
-
-    function personal_listAccounts(){
-        return $this->ether_request(__FUNCTION__);
-    }
-
     function personal_unlockAccount($address,$passphrase,$duration=300){
         return $this->ether_request(__FUNCTION__, array($address,$passphrase,$duration));
-    }
-
-    function personal_lockAccount($address){
-        return $this->ether_request(__FUNCTION__, array($address));
-    }
-
-    function personal_ecRecover($message, $signature){
-        return $this->ether_request(__FUNCTION__, array($message,$signature));
-    }
-
-    function personal_importRawKey($keydata, $passphrase){
-        return $this->ether_request(__FUNCTION__, array($keydata,$passphrase));
     }
 
     function personal_sendTransaction(EthereumTransaction $transaction,$passphrase){
